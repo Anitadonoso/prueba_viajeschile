7764abb0a007d6a10f49d528ec95086e65d5a217
Anita <anita.donosok@gmail.com>
2022-09-23 22:47:01 -0300

    finalización

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,29 +1,30 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    
-    $('#btn-contactar').click(function(){
-        alert("Tu información ha sido enviada correctamente.")
-    });
-    $(".containerparrafos").click(function(){
-        $(".containerparrafos").slideToggle(1500); 
-        $(".containerparrafos").toggle('show');
-    });
-    $(".esconder-mostrarmontaña").click(function(){
-        $(".esconder-mostrarmontaña").slideToggle(1500); 
-        $(".esconder-mostrarmontaña").toggle('show');
-    });
-    $(".esconder-mostrarruta").click(function(){
-        $(".esconder-mostrarruta").slideToggle(1500); 
-        $(".esconder-mostrarruta").toggle('show');
+$(document).ready(function () {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+        /*mensaje de alerta*/
+        $('#btn-contactar').click(function () {
+            alert("Tu información ha sido enviada correctamente.")
+        });
+        /*esconder párrafos*/
+        $(".esconder-mostraravion").click(function () {
+            $(".esconder-mostraravion").toggle();
+        });
+        $(".esconder-mostrarmontaña").click(function () {
+            $(".esconder-mostrarmontaña").toggle();
+        })
+        $(".esconder-mostrarruta").click(function () {
+            $(".esconder-mostrarruta").toggle();
+        });
     });
      })
-     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    /*smooth scroll*/
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             $('html,body').animate({
-                'scrollTop': $($(this).attr('href')).offset().top+'px'
+                'scrollTop': $($(this).attr('href')).offset().top + 'px'
             }, 5000);
-            
-    
+
+
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
             });
